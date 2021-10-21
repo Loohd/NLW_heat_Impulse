e74d16b35b6ad6c2b0d9b1f8b89ddf34416810ff
Bruno Rodrigues <brunirodrigues102009@hotmail.com>
2021-10-20 23:09:33 -0300

    route message ok

--- src/controllers/AuthenticateUserController.ts
+++ src/controllers/AuthenticateUserController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { AuthenticateUserService } from "../AuthenticateUserService";
+import { AuthenticateUserService } from "../services/AuthenticateUserService";
 
 
 
